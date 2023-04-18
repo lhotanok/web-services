d3109934aa39be481e66bfa6cd9e2af9153e3c88
Kristýna Lhoťanová <kristyna.lhotanova@gmail.com>
2023-04-18 18:31:53 +0200

    Add basic swagger definition manually after too many unsuccessful tries to generate it from code

--- 08-rest/TripPlannerRestApi/src/main/java/org/example/TripPlannerApplication.java
+++ 08-rest/TripPlannerRestApi/src/main/java/org/example/TripPlannerApplication.java
@@ -3,6 +3,5 @@ package org.example;
 import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 
-
 @ApplicationPath("/rest")
 public class TripPlannerApplication extends ResourceConfig { }
